8738e2f0c7c70e50e9e61a7391be7e47f739067e
wellington <wellington281482@gmail.com>
2023-10-29 18:41:30 -0300

    New files

---
+++ assembly.asm
@@ -0,0 +1,8 @@
+bits 64
+
+section .text
+
+global assembly
+assembly:
+  mov eax, 777
+  ret
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int assembly(void);
+
+int main(void){
+  printf("Resultado: %d\n", assembly());
+  return 0;
+}
